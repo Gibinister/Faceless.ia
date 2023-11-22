95b2b90b00304bdf4c6377f218e4635bee0d5431
Gibinister <48371980+Gibinister@users.noreply.github.com>
2023-11-22 16:44:51 -0300

    Update Integracao.py

--- src/main/Integracao.py
+++ src/main/Integracao.py
@@ -6,7 +6,7 @@ def respostaIA(prompt):
     textoResposta = openai.Completion.create(
         engine="text-davinci-003",
         prompt= "Caso tenha de referir a si mesmo, invés de se chamar ChatGPT ou outra coisa, chame-se de Faceless.IA. Responda o seguinte prompt sucintamente, (" + prompt + ")",
-        max_tokens=200
+        max_tokens=600
     )
     
     return textoResposta['choices'][0]['text']
